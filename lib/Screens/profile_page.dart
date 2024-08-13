eb31992abf1e404b7b9942c3b4f8573c244d040f
[Nguyet12] <92924131+Nguyet12@users.noreply.github.com>
2024-08-13 15:59:27 +0900

    마이페이지에서 사용자 정보를 가져 와서 표시하기

--- lib/Screens/profile_page.dart
+++ lib/Screens/profile_page.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/Screens/login_page.dart';
-import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'dart:io';
-import 'package:shared_preferences/shared_preferences.dart';
-import '../Services/AuthService.dart'; // AuthService를 import 합니다.
+import 'package:image_picker/image_picker.dart';
+import 'login_page.dart';
+import '../Services/AuthService.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProfilePage(),
+    );
+  }
+}
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -13,22 +31,60 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  String nickname = "매미매미"; // 닉네임
-  String birthdate = "2002년 10월 28일"; // 생년월일
-  String gender = "여성"; // 성별
-  final ImagePicker _picker = ImagePicker(); // 이미지 선택기
+  String nickname = "알 수 없음"; // 닉네임
+  String birthday = "지정되지 않음"; // 생년월일
+  String gender = "지정되지 않음"; // 성별
+  String email = "지정되지 않음"; // 이메일
   String? _profileImagePath = 'assets/profile_image.png'; // 프로필 이미지 경로
+  final ImagePicker _picker = ImagePicker(); // 이미지 선택기
 
-  // 날짜 선택 함수
-  Future<void> _selectDate(BuildContext context, Function onSave) async {
-    final DateTime? picked = await showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2101),
-    );
-    if (picked != null && picked != DateTime.now()) {
-      onSave(picked);
+  @override
+  void initState() {
+    super.initState();
+    fetchProfile(); // 페이지 로드 시 프로필 정보를 가져옵니다.
+  }
+
+  // Spring Boot API 호출
+  Future<void> fetchProfile() async {
+    final token = await AuthService.getToken();
+
+    if (token == null) {
+      print("토큰이 없어 사용자가 로그아웃되었습니다.");
+      return;
+    }
+
+    try {
+      final response = await http.get(
+        Uri.parse('http://192.168.56.1:8081/account/profile'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        // UTF-8로 데이터를 디코딩합니다.
+        final data = jsonDecode(utf8.decode(response.bodyBytes));
+        print(data); // 서버 응답을 출력하여 확인
+        setState(() {
+          nickname = data['userId'] ?? '알 수 없음';
+          birthday = data['birthday'] ?? '지정되지 않음';
+          gender = data['gender'] ?? '지정되지 않음';
+          if (gender == "F") {
+            gender = "남성";
+          } else {
+            gender = "여성";
+          }
+          email = data['email'] ?? '지정되지 않음';
+          _profileImagePath =
+              data['profileImagePath'] ?? 'assets/profile_image.png';
+        });
+      } else {
+        print('프로필 로드 실패: ${response.statusCode}');
+        throw Exception('프로필을 불러오는 데 실패했습니다.');
+      }
+    } catch (e) {
+      print('프로필을 가져오는 중 오류 발생: $e');
     }
   }
 
@@ -43,19 +99,24 @@ class _ProfilePageState extends State<ProfilePage> {
   }
 
   // 로그아웃 함수
+  final AuthService _authService = AuthService();
   Future<void> _logout() async {
     try {
-      final success = AuthService().logout();
-      if (await success) {
-        await AuthService.clearToken();
-        // 로그아웃 후 적절한 화면으로 리디렉션
+      final success = await _authService.logout(); // 로그아웃 API 호출
+
+      if (success) {
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(builder: (context) => LoginPage()),
         );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('로그아웃에 실패했습니다. 다시 시도해주세요.'),
+          ),
+        );
       }
     } catch (e) {
-      // 오류 처리
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
           content: Text('로그아웃 실패: ${e.toString()}'),
@@ -78,7 +139,7 @@ class _ProfilePageState extends State<ProfilePage> {
         title: const Text(
           '마이페이지',
           style: TextStyle(fontFamily: 'Quicksand', color: Colors.white),
-        ), // 마이페이지 글자 흰색
+        ),
         backgroundColor: const Color.fromARGB(255, 173, 216, 230), // 앱바 색상 설정
       ),
       body: Padding(
@@ -92,9 +153,11 @@ class _ProfilePageState extends State<ProfilePage> {
                   CircleAvatar(
                     radius: 40,
                     backgroundImage: _profileImagePath != null
-                        ? FileImage(File(_profileImagePath!))
-                        : const AssetImage('assets/profile_image.png')
-                            as ImageProvider,
+                        ? (_profileImagePath!.startsWith('http')
+                                ? NetworkImage(_profileImagePath!)
+                                : FileImage(File(_profileImagePath!)))
+                            as ImageProvider
+                        : const AssetImage('assets/profile_image.png'),
                   ),
                   // 프로필 이미지 변경 아이콘
                   Positioned(
@@ -117,28 +180,17 @@ class _ProfilePageState extends State<ProfilePage> {
               ),
             ),
             const SizedBox(height: 16),
-            // 프로필 필드 (닉네임, 생년월일, 성별)
-            buildProfileField("이름 또는 닉네임", nickname, (newValue) {
-              setState(() {
-                nickname = newValue;
-              });
-            }),
-            buildProfileField("생년월일", birthdate, (newValue) {
-              setState(() {
-                birthdate = newValue;
-              });
-            }),
-            buildProfileField("성별", gender, (newValue) {
-              setState(() {
-                gender = newValue;
-              });
-            }),
+            // 프로필 필드 (유저네임, 생년월일, 성별, 이메일)
+            buildProfileField("유저네임", nickname),
+            buildProfileField("생년월일", birthday),
+            buildProfileField("성별", gender),
+            buildProfileField("이메일", email),
             const SizedBox(height: 16),
             // 로그아웃 버튼
             ElevatedButton(
               onPressed: () async {
                 await _logout(); // 로그아웃 처리
-              },
+              }, // 매미매미 은혜은혜 바보 멍청이 못생긴 똥개 맨날 뚱뚱이라고 놀리고 시러! 미워!
               style: ElevatedButton.styleFrom(
                 backgroundColor:
                     const Color.fromARGB(255, 173, 216, 230), // 앱바와 동일한 색상
@@ -164,8 +216,7 @@ class _ProfilePageState extends State<ProfilePage> {
   }
 
   // 프로필 필드 위젯 생성
-  Widget buildProfileField(
-      String label, String value, Function(String) onEdit) {
+  Widget buildProfileField(String label, String value) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -179,7 +230,7 @@ class _ProfilePageState extends State<ProfilePage> {
         ),
         const SizedBox(height: 4),
         Container(
-          color: Colors.white, // 필드 배경색을 흰색으로 설정
+          color: Colors.white,
           child: Row(
             children: [
               Expanded(
@@ -192,57 +243,11 @@ class _ProfilePageState extends State<ProfilePage> {
                   ),
                 ),
               ),
-              IconButton(
-                icon: const Icon(Icons.edit),
-                onPressed: () {
-                  _showEditDialog(context, label, value, onEdit); // 수정 다이얼로그 표시
-                },
-              ),
             ],
           ),
         ),
         const Divider(),
       ],
-    );
-  }
-
-  // 수정 다이얼로그 표시 함수
-  void _showEditDialog(BuildContext context, String label, String value,
-      Function(String) onEdit) {
-    TextEditingController controller = TextEditingController(text: value);
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('$label 수정',
-              style: const TextStyle(fontFamily: 'Quicksand')),
-          content: TextField(
-            controller: controller,
-            decoration: InputDecoration(
-              hintText: label,
-              hintStyle: const TextStyle(fontFamily: 'Quicksand'),
-            ),
-            style: const TextStyle(fontFamily: 'Quicksand'),
-          ),
-          actions: [
-            TextButton(
-              child:
-                  const Text('취소', style: TextStyle(fontFamily: 'Quicksand')),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child:
-                  const Text('저장', style: TextStyle(fontFamily: 'Quicksand')),
-              onPressed: () {
-                onEdit(controller.text); // 변경된 값 저장
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
     );
   }
 }
